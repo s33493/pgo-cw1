584b77e6a2fa6efb4c40bf296c3ae1696abde62c
s33493 <s33493@pjwstk.edu.pl>
2025-03-06 09:38:22 +0100

    Change Main.java

--- Cwiczenia1/src/Main.java
+++ Cwiczenia1/src/Main.java
@@ -2,6 +2,10 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        
+        System.out.println("Hello in a second semester!");
+
+    }
+    public static int metoda1(int a, int b) {
+        return a + b;
     }
 }
